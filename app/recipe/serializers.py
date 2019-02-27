849bbfd3afe0c8afe7668e0c5af26eddfa697d2d
sajj <sajjfarahani@gmail.com>
2019-02-27 10:14:39 +0000

    Adding Recipe viewset

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -39,4 +39,10 @@ class RecipeSerializer(serializers.ModelSerializer):
         read_only_fields = ('id',)
 
 
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serialize recipe detail"""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
+
+
 
